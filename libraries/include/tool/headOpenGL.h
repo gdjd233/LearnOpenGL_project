0b487d98713914dfc9f375a4b8e18c8a575036b8
wmm <wmm@q.com>
2025-05-09 21:35:54 +0800

    Initial commit

---
+++ libraries/include/tool/headOpenGL.h
@@ -0,0 +1,50 @@
+
+// ≤‚ ‘
+int test(int argc, char* argv[]);
+
+// learnOpenGL
+int solve_01(int argc, char* argv[]);
+
+int solve_02(int argc, char* argv[]);
+int solve_02_01(int argc, char* argv[]);
+
+int solve_03(int argc, char* argv[]);
+int solve_03_01(int avgc, char* argv[]);
+int solve_03_02(int avgc, char* argv[]);
+
+int solve_04(int argc, char* argv[]);
+int solve_04_01(int argc, char* argv[]);
+
+int solve_05(int argc, char* argv[]); 
+
+int solve_06(int argc, char* argv[]);
+int solve_06_01(int argc, char* argv[]);
+
+int solve_07(int argc, char* argv[]);
+int solve_07_01(int argc, char* argv[]);
+int solve_07_02(int argc, char* argv[]);
+
+
+
+// ¡∑œ∞
+int work_02_01(int argc, char* argv[]);
+int work_02_02(int argc, char* argv[]);
+int work_02_03(int argc, char* argv[]);
+
+int work_03_01(int avgc, char* argv[]);
+int work_03_02(int avgc, char* argv[]);
+int work_03_03(int avgc, char* argv[]);
+
+int work_04_01(int avgc, char* argv[]);
+int work_04_02(int avgc, char* argv[]);
+int work_04_03(int avgc, char* argv[]);
+int work_04_04(int avgc, char* argv[]);
+
+int work_05_01(int avgc, char* argv[]);
+int work_05_02(int avgc, char* argv[]);
+
+int work_06_01(int avgc, char* argv[]);
+int work_06_02(int avgc, char* argv[]);
+
+int work_07_01(int avgc, char* argv[]);
+int work_07_02(int avgc, char* argv[]);
